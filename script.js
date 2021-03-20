47c0042b05feb017d1b3186f1df71f2033e98150
Timur <timatemirkhanov1995@gmail.com>
2021-03-20 22:58:19 +0700

    lesson2

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-const goods = [
-  { title: "Shirt", price: 150 },
-  { title: "Socks", price: 50 },
-  { title: "Jacket", price: 350 },
-  { title: "Shoes", price: 250 },
-];
+class ProductsList {
+  constructor(container = ".goods-list") {
+    this.container = container;
+    this.goods = [];
+    this.allProducts = [];
+    this._fetchProducts();
+  }
 
-const renderGoodsItem = (item, img = "https://placehold.it/200x150") => {
-  return `<div class="goods-item">
-  <img src="${img}">
-  <h3>${item.title}</h3>
-  <p>${item.price}</p>
-  <button class="buy-btn">Купить</button>
-  </div>`;
-};
+  _fetchProducts() {
+    this.goods = [
+      { id: 1, title: "Shirt", price: 150 },
+      { id: 2, title: "Socks", price: 50 },
+      { id: 3, title: "Jacket", price: 350 },
+      { id: 4, title: "Shoes", price: 250 },
+    ];
+  }
+  sumPrices() {
+    let total = this.goods.reduce((sum, good) => sum + good.price, 0);
+    return total;
+  }
+  render() {
+    const block = document.querySelector(this.container);
+    for (let product of this.goods) {
+      const productObj = new ProductItem(product);
+      this.allProducts.push(productObj);
+      block.insertAdjacentHTML("beforeend", productObj.render());
+      //            block.innerHTML += productObj.render();
+    }
+  }
+}
 
-const renderGoodsList = (list) => {
-  document.querySelector(".goods-list").innerHTML = list
-    .map((item) => renderGoodsItem(item))
-    .join("");
-};
+class ProductItem {
+  constructor(product, img = "https://placehold.it/200x150") {
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = img;
+  }
 
-renderGoodsList(goods);
+  render() {
+    return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`;
+  }
+}
+
+let list = new ProductsList();
+list.render();
+list.sumPrices();
+
+class Basket {
+  addGoods() {}
+  removeGoods() {}
+  changeGoods() {}
+}
+
+class ElemBasket {}
