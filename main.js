3a0376ed4c5a16731e16d81153fac8e098b625a0
Timur <timatemirkhanov1995@gmail.com>
2021-04-05 19:17:06 +0700

    lesson6

--- main.js
+++ main.js
@@ -47,6 +47,31 @@ const app = new Vue({
   },
 });
 
+Vue.component("goods-list", {
+  props: {
+    goods: {},
+  },
+  template: `
+    <div class="goods-list">
+      <goods-item v-for="good in goods" :good="good"></goods-item>
+    </div>
+  `,
+});
+
+Vue.component("goods-item", {
+  props: ["good"],
+  template: `
+    <div class="goods-item">
+      <h3>{{ good.product_name }}</h3>
+      <p>{{ good.price }}</p>
+    </div>
+  `,
+});
+
+Vue.component("search", {});
+
+Vue.component("basket", {});
+
 class List {
   constructor(url, container, list = list2) {
     this.container = container;
